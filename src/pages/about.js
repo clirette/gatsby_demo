f05cf166ec7f698446dd69f9f187e2b00c44ca4c
Chase Lirette <clirette@dxc.com>
2019-04-29 17:45:07 -0500

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const AboutPage = () => (
+  <Layout>
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
+        voluptatibus veniam ipsa illo, corrupti labore est laboriosam
+        consectetur. Deleniti mollitia neque doloribus quod in iste odit libero
+        eius, aliquid reprehenderit dolorem eum dolores possimus, voluptates
+        fuga? Officiis est eum veniam?
+      </p>
+    </div>
+  </Layout>
+)
+export default AboutPage
---
+++ src/pages/services.js
@@ -0,0 +1,26 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ServicesPage = () => (
+  <Layout>
+    <div>
+      <h1>Our Services</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
+        voluptatibus veniam ipsa illo, corrupti labore est laboriosam
+        consectetur. Deleniti mollitia neque doloribus quod in iste odit libero
+        eius, aliquid reprehenderit dolorem eum dolores possimus, voluptates
+        fuga? Officiis est eum veniam?
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
+        voluptatibus veniam ipsa illo, corrupti labore est laboriosam
+        consectetur. Deleniti mollitia neque doloribus quod in iste odit libero
+        eius, aliquid reprehenderit dolorem eum dolores possimus, voluptates
+        fuga? Officiis est eum veniam?
+      </p>
+    </div>
+  </Layout>
+)
+
+export default ServicesPage
